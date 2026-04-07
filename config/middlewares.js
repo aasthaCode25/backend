5c05a4800b7827ce7d0d01c409db6d7e371ab509
Alsycode <120390865+Alsycode@users.noreply.github.com>
2026-04-07 18:42:02 +0530

    first commit

---
+++ config/middlewares.js
@@ -0,0 +1,32 @@
+module.exports = [
+  'strapi::logger',
+  'strapi::errors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+ {
+    name: 'strapi::cors',
+    config: {
+      origin: ['https://aasthaparivaar.org','https://69.62.75.177', 'http://69.62.75.177:1337','https://www.aasthaparivaar.org'], // Add your Next.js and Strapi origins
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization'],
+    },
+  },
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
